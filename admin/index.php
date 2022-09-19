0608a5ddec3f320c9b317d0bc8e06ee4cb1bb205
rohit <rhtry.tech@gmail.com>
2022-09-19 23:42:33 +0530

    adding admin

---
+++ admin/index.php
@@ -0,0 +1,91 @@
+<!-- Main CMS/ Admin file  -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="../css/admin.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"
+      integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
+      crossorigin="anonymous"
+    />
+
+    <!-- Optional theme -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css"
+      integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ"
+      crossorigin="anonymous"
+    />
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
+      integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+      crossorigin="anonymous"
+    ></script>
+
+    <title>Food Ordering App - Home Page</title>
+  </head>
+  <body>
+    <!-- Menu Section -->
+    <div class="menu"> 
+        <div class="wrapper">
+            <ul> 
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Main Content Section-->
+    <div class="main-content"> 
+        <div class="wrapper">
+            
+            <h2 style="text-align: center">Main Dashboard</h2>
+
+            <div class="col-4">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="clearfix"></div>
+
+        </div>
+    </div>
+    <!-- Footer Section -->
+    <div class="footer"> 
+        <div class="wrapper">
+            Copyright Text
+        </div>
+    </div>
+  </body>
+</html>
