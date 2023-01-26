0f1a32764d2168631a07d0b2279c025342826982
rohit <rhtry.tech@gmail.com>
2023-01-26 22:38:55 +0530

    adding files !

--- categories.php
+++ categories.php
@@ -1,152 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <!-- Important to make website responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
-
     <!-- Link our CSS file -->
     <link rel="stylesheet" href="./css/style.css">
+
 </head>
 
 <body>
 
-<?php include './partials/menu.php'; ?>
+    <!-- navbarPortion -->
+<?php include './front-end-partials/menu.php'; ?>
 
     <!-- CAtegories Section Starts Here -->
     <section class="categories">
         <div class="container">
-            <h2 class="text-center">Explore Foods</h2>
+            <h2 class="text-center">Explore Food Categories</h2>
+
+                        <!-- messageSessions -->
+                        <?php if (isset($_SESSION['no-categories-available'])) {
+                            echo $_SESSION['no-categories-available'];
+                            // Ending session
+                            unset($_SESSION['no-categories-available']);
+                        } ?>
+
+
+            <!-- displayingAllTheAvailableCategory -->
+
+            <?php
+            $sql = 'SELECT * FROM tbl_category WHERE active="Yes"';
+            $res = mysqli_query($conn, $sql);
+            $count = mysqli_num_rows($res);
+
+            // checkIfCategoriesAvailable?
+            if ($count > 0) {
+                // categoriesAvailable
+                while ($rows = mysqli_fetch_assoc($res)) {
+
+                    $id = $rows['id'];
+                    $title = $rows['title'];
+                    $image_name = $rows['image_name'];
+                    ?>
 
             <a href="category-foods.html">
-            <div class="box-3 float-container">
-                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Pizza</h3>
-            </div>
+                <div class="box-3 float-container">
+                                        <!-- ifImageIsAvailableThenOnlyDisplay -->
+                                        <?php if ($image_name == '') {
+                                            echo 'Image is not available !';
+                                        } else {
+                                             ?>
+                                             <!-- imageAvailable -->
+                            <img src="<?php echo HOMEURL; ?>images/category/<?php echo $image_name; ?>" alt="<?php echo $title; ?>" class="img-responsive img-curve" width="400" height="200">
+                    <?php
+                                        } ?>
+                    <h3 class="float-text text-white"><?php echo $title; ?></h3>
+                </div>
             </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Burger</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Momo</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Pizza</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Burger</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Momo</h3>
-            </div>
-            </a>
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Pizza</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Burger</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Momo</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Pizza</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Burger</h3>
-            </div>
-            </a>
-
-            <a href="#">
-            <div class="box-3 float-container">
-                <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
-
-                <h3 class="float-text text-white">Momo</h3>
-            </div>
-            </a>
-
-
+                    <?php
+                }
+            } else {
+                // categoriesNotAvailable
+                $_SESSION['no-categories-available'] = 'No Categories Found !';
+                header('location:' . HOMEURL);
+            }
+            ?>
 
             <div class="clearfix"></div>
         </div>
     </section>
     <!-- Categories Section Ends Here -->
 
-
-    <!-- social Section Starts Here -->
-    <section class="social">
-        <div class="container text-center">
-            <ul>
-                <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
-                </li>
-                <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
-                </li>
-                <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
-                </li>
-            </ul>
-        </div>
-    </section>
-    <!-- social Section Ends Here -->
-
-    <!-- footer Section Starts Here -->
-    <section class="footer">
-        <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Vijay Thapa</a></p>
-        </div>
-    </section>
-    <!-- footer Section Ends Here -->
+    <!-- footerPortion -->
+    <?php include './front-end-partials/footer.php'; ?>
 
 </body>
 </html>
