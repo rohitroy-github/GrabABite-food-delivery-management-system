254156bf39688c9ff57285f474ee3250ad6755ec
rohit <rhtry.tech@gmail.com>
2023-02-07 00:56:07 +0530

    modifications !

--- admin/index.php
+++ admin/index.php
@@ -22,43 +22,52 @@
     <div class="main-content"> 
         <div class="wrapper">
             
-            <h2 style="text-align: center">Main Dashboard</h2>
+            <h2 style="text-align: center">Dashboard</h2>
 
             <!-- Login Message -->
 
+            <?php if (isset($_SESSION['login'])) {
+                echo $_SESSION['login'];
+                unset($_SESSION['login']);
+            } ?>
+
+            <div class="col-4">
+              <?php
+              $sql_category = 'SELECT * FROM tbl_category';
+              $res_category = mysql_query($conn, $sql_category);
+              $count_category = mysqli_num_rows($res_category);
+              ?>
+                <h1><?php echo $count_category; ?></h1>
+                <br />
+                Active Categories
+            </div>
+
+            <div class="col-4">
             <?php
-
-              if(isset($_SESSION['login'])){ 
-
-                echo $_SESSION['login']; 
-                unset($_SESSION['login']); 
-
-              } 
-
+            $sql_food = 'SELECT * FROM tbl_food';
+            $res_food = mysql_query($conn, $sql_food);
+            $count_food = mysqli_num_rows($res_food);
             ?>
+                <h1><?php echo $count_food; ?></h1>
+                <br />
+                Food Items
+            </div>
 
             <div class="col-4">
-                <h1>5</h1>
+            <?php
+            $sql_order = 'SELECT * FROM tbl_order';
+            $res_order = mysql_query($conn, $sql_order);
+            $count_order = mysqli_num_rows($res_order);
+            ?>
+                <h1><?php echo $count_order; ?></h1>
                 <br />
-                Categories
+                Total Orders
             </div>
 
             <div class="col-4">
                 <h1>5</h1>
                 <br />
-                Categories
-            </div>
-
-            <div class="col-4">
-                <h1>5</h1>
-                <br />
-                Categories
-            </div>
-
-            <div class="col-4">
-                <h1>5</h1>
-                <br />
-                Categories
+                Total Revenue
             </div>
 
             <div class="clearfix"></div>
